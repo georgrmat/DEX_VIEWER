d8f040220badbd802c48a93008d409a5c41967d8
Georges <grahal.science@gmail.com>
2024-01-13 22:12:25 +0100

    commentaries

--- app.py
+++ app.py
@@ -4,58 +4,62 @@ import streamlit as st
 import time
 import json
 
+# Load token addresses from a file
 with open("data/token_adresses.txt", "r") as f:
     tokens_str = f.read()
     
+# Parse JSON data from the file
 tokens = json.loads(tokens_str)
 
-
+# Streamlit app setup
 st.title("PulseChain DEX Viewer")
 st.sidebar.title("Parameters")
-crypto = st.sidebar.radio("I have", ["HEX", "PLS", "PLSX"])
+crypto = st.sidebar.radio("I possess", ["HEX", "PLS", "PLSX"])
 nombre = st.sidebar.number_input("Number of tokens", value = 100000)
 
 token_prices = {}
 token_changes = {}
 
+# Button to trigger data retrieval and analysis
 launch_button = st.sidebar.button("Launch")
 
+# Function to determine background color based on percentage change
 def bgcolor_positive_or_negative(value):
     bgcolor = "#b1532d" if float(value.replace("%", "")) < 0 else "lightgreen"
     return f"background-color: {bgcolor}; opacity: 0.1"
 
-st.header("Variations")
-
-while True:
-    for pair in tokens:
-        url = f"https://api.dexscreener.com/latest/dex/pairs/pulsechain/{tokens[pair]}"
-        response = requests.get(url)
-        token_changes[pair] = response.json()["pairs"][0]["priceChange"]
-    
-    
-    
-    df_change = pd.DataFrame(token_changes).T
-    df_change.reset_index(drop = False, inplace = True, names = ["Token"])
-    
-    for column in ["m5", "h1", "h6", "h24"]:
-        df_change[column] = df_change[column].apply(lambda x: str(x) + "%")
-    styled_df_change = df_change.style.applymap(bgcolor_positive_or_negative, subset = ["m5", "h1", "h6", "h24"])
-    st.dataframe(styled_df_change, use_container_width=True, hide_index = True)
-    time.sleep(5)
-    st.cache_data.clear()
-
-
+# Execute when the Launch button is clicked
 if launch_button:
+    # Retrieve data for each token pair
     for pair in tokens:
         url = f"https://api.dexscreener.com/latest/dex/pairs/pulsechain/{tokens[pair]}"
         response = requests.get(url)
+        
+        # Extract price and price change information
         price = [response.json()["pairs"][0]["priceUsd"]]
         token_prices[pair] = price
+        token_changes[pair] = response.json()["pairs"][0]["priceChange"]
+        
+    # Display variations in a DataFrame
+    st.header("Variations")
+    df_change = pd.DataFrame(token_changes).T
+    df_change.reset_index(drop=False, inplace=True, names=["Token"])
     
+    # Format percentage columns
+    for column in ["m5", "h1", "h6", "h24"]:
+        df_change[column] = df_change[column].apply(lambda x: str(x) + "%")
+    
+    # Apply styling to the DataFrame
+    styled_df_change = df_change.style.applymap(bgcolor_positive_or_negative, subset=["m5", "h1", "h6", "h24"])
+    st.dataframe(styled_df_change, use_container_width=True, hide_index=True)
+    
+    # Display number of tokens and prices in a DataFrame
     st.header("Number of tokens and prices")
-    df_tokens = pd.DataFrame.from_dict(token_prices, orient = "index")
-    df_tokens.reset_index(drop = False, inplace = True)
+    df_tokens = pd.DataFrame.from_dict(token_prices, orient="index")
+    df_tokens.reset_index(drop=False, inplace=True)
     df_tokens.columns = ["Token", "PriceUSD"]
-    df_tokens["NumberOfTokens"] = df_tokens.apply(lambda x: nombre * float(token_prices[crypto][0]) / float(x["PriceUSD"]), axis = 1)
-    st.dataframe(df_tokens, use_container_width=True, hide_index = True)
+    
+    # Calculate number of tokens and total value
+    df_tokens["NumberOfTokens"] = df_tokens.apply(lambda x: nombre * float(token_prices[crypto][0]) / float(x["PriceUSD"]), axis=1)
+    st.dataframe(df_tokens, use_container_width=True, hide_index=True)
     st.write("Total value:", nombre * float(token_prices[crypto][0]), "$")
